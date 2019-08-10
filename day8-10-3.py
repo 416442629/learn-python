493f2151b689338349037bb62819555145b15b2a
南瓜马车 <416442629@qq.com>
2019-08-10 11:23:47 +0800

    递归+嵌套函数

---
+++ day8-10-3.py
@@ -0,0 +1,20 @@
+#嵌套函数
+def factorial1(n):
+    def recurse(n,product):
+        if n==1:
+            return product
+        else:
+            return recurse(n-1,n*product)
+    return recurse(n,2)
+def factorial(n,product):
+    print(n,product)
+    if n == 1:
+        return product
+    else:
+        return factorial(n - 1, n * product)
+
+
+
+print(factorial(10,2))
+print('-----------')
+print(factorial1(10))
